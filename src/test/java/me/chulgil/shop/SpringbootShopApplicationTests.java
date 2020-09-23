6da92ca8e6fcf4f66715ea85a8af878a86e118a6
CG.Lee <contact@chulgil.me>
2020-09-23 22:17:22 +0900

    first init

---
+++ src/test/java/me/chulgil/shop/SpringbootShopApplicationTests.java
@@ -0,0 +1,13 @@
+package me.chulgil.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
